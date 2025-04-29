4f0a30a1b6c90b9a6155c5bb471c4aad1ecaa24c
y-elmaa <elmaaroufiyahya19@gmail.com>
2025-04-29 15:07:26 +0100

    Show a confirmation dailog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,31 @@
-import { Issus } from '@/app/generated/prisma'
-import { Button } from '@radix-ui/themes'
-import React from 'react'
+import { Issus } from "@/app/generated/prisma";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import React from "react";
 
-const DeleteIssueButton = ({issueId}:{issueId:number}) => {
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color='red'>Delete issue</Button>
-  )
-}
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirme Deletion</AlertDialog.Title>
+        <AlertDialog.Description>Are you sure?</AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="soft" color="red">
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
 
-export default DeleteIssueButton
+export default DeleteIssueButton;
