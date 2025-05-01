9a7c4fc00e21e8a700618fe5b3bcb92d90b9c8b8
y-elmaa <elmaaroufiyahya19@gmail.com>
2025-05-01 16:47:21 +0100

    Enhance the API to assign issues

--- app/ValidationSchemas.tsx
+++ app/ValidationSchemas.tsx
@@ -4,3 +4,17 @@ export const issueSchema = z.object({
   title: z.string().min(1, "Title is required").max(255),
   description: z.string().min(1, "Descriptio is required"),
 });
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Descriptio is required")
+    .max(65535)
+    .optional(),
+  assigneToUserId: z
+    .string()
+    .min(1, "assigneeToUserId is requerid")
+    .max(255)
+    .optional()
+    .nullable(),
+});
--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,8 +1,10 @@
 import AuthOption from "@/app/auth/AuthOption";
-import { issueSchema } from "@/app/ValidationSchemas";
+import { patchIssueSchema } from "@/app/ValidationSchemas";
 import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+
+//Update
 
 export async function PATCH(
   request: NextRequest,
@@ -11,10 +13,21 @@ export async function PATCH(
   const session = await getServerSession(AuthOption);
   if (!session) return NextResponse.json({}, { status: 401 });
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+  const { assigneToUserId, title, description } = body;
+  if (assigneToUserId) {
+    const user =await prisma.user.findUnique({
+      where: {
+        id: assigneToUserId,
+      },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
+    }
+  }
   const issue = await prisma.issus.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -22,10 +35,13 @@ export async function PATCH(
     return NextResponse.json({ error: "issue not exist" }, { status: 404 });
   const updatedIssue = await prisma.issus.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description },
+    data: { title, description, assigneToUserId },
   });
   return NextResponse.json(updatedIssue);
 }
+
+// Delete
+
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
