45549738842c5e7955da4fbb3145ed15b4df47ec
y-elmaa <elmaaroufiyahya19@gmail.com>
2025-05-05 16:53:52 +0100

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+export default async function Home(props:{searchParams:Promise<{page:string}>}) {
+  const {page} = await props.searchParams
   return (
-    <Pagination itemCount={100} currentPage={10} pageSize={10}/>
+    <Pagination itemCount={100} currentPage={parseInt(page)} pageSize={10}/>
   );
 }
