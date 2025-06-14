1c8771ea8f5edebf0d173c77b285c9a50d207b68
y-elmaa <elmaaroufiyahya19@gmail.com>
2025-06-14 12:28:06 +0100

    fix avatar not showing

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -77,6 +77,7 @@ const AuthStatus = () => {
             size="2"
             radius="full"
             className="cursor-pointer"
+            referrerPolicy="no-referrer"
           />
         </DropdownMenu.Trigger>
         <DropdownMenu.Content>
