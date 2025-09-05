874eaf76227ebfb1f0d2d44c5fa874ab31b4f31e
y-elmaa <145350222+y-elmaa@users.noreply.github.com>
2025-09-05 16:57:36 +0100

    Update client.ts

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,20 +1,7 @@
-import { PrismaClient } from "@/app/generated/prisma";
-
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    __internal: {
-      usePreparedStatements: false, // disable prepared statements
-    },
-  });
-};
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
+import { PrismaClient } from "@/app/generated/prisma"; 
+const prismaClientSingleton = () => { return new PrismaClient(); }; 
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>; 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClientSingleton | undefined; }; 
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton(); 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
